62292df3665e54eb539cf4b5b02338ecbab9b0ab
Vu Anh Le <anhlv@ymail.com>
2018-08-10 05:38:04 +1000

    add method

--- src/comp3506/assn1/adts/BoundedCube.java
+++ src/comp3506/assn1/adts/BoundedCube.java
@@ -20,7 +20,55 @@ public class BoundedCube<T> implements Cube<T> {
 	 * @param height  Maximum size in the 'z' dimension.
 	 * @throws IllegalArgumentException If provided dimension sizes are not positive.
 	 */
+	
+	int length, breadth, height;
+	
+			
 	public BoundedCube(int length, int breadth, int height) throws IllegalArgumentException {
+		this.length = length;
+		this.breadth = breadth;
+		this.height= height;
+	}
+
+	@Override
+	public void add(int x, int y, int z, T element) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T get(int x, int y, int z) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IterableQueue<T> getAll(int x, int y, int z) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isMultipleElementsAt(int x, int y, int z) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(int x, int y, int z, T element) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeAll(int x, int y, int z) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
 		
 	}
 	
--- src/comp3506/assn1/adts/TraversableQueue.java
+++ src/comp3506/assn1/adts/TraversableQueue.java
@@ -1,6 +1,39 @@
 package comp3506.assn1.adts;
 
+import java.util.Iterator;
 
 public class TraversableQueue<T> implements IterableQueue<T> {
+
+	int size;
+	
+	
+	
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enqueue(T element) throws IllegalStateException {
+		this.size +=1;
+		
+	}
+
+	@Override
+	public T dequeue() throws IndexOutOfBoundsException {
+		if (this.size > 0) {
+			this.size -=1;
+
+		} else {
+			return null;	
+		}
+
+	}
+
+	@Override
+	public int size() {
+		return this.size;
+	}
 
 }
